17ab247963ddbf796e37aa01f086148fb7752122
TimMindat <timmind@aiesec.net>
2024-11-26 01:16:24 +0200

    3

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -12,6 +12,10 @@ i18n
     interpolation: {
       escapeValue: false,
     },
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage'],
+    },
   });
 
 export default i18n;
